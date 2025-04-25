af580ca4da337b43326f9a30bf79cd40d8844905
alessandroibes <alessandro.ibes@gmail.com>
2025-04-25 10:21:20 -0300

    Add tests with pytest: create

---
+++ tests.py
@@ -0,0 +1,22 @@
+import pytest
+import requests
+
+
+BASE_URL = "http://localhost:5000"
+tasks = []
+
+
+def test_create_task():
+    new_task_data = {
+        "title": "Nova tarefa",
+        "description": "Descrição da nova tarefa"
+    }
+
+    response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert "message" in response_json
+    assert "id" in response_json
+
+    tasks.append(response_json["id"])
